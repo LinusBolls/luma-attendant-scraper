6ee407b815e35022834dd2911b1a68ca597fa5eb
Linus Bolls <linus.bolls@gmail.com>
2025-01-19 17:25:16 +0100

    feat: /luma-events endpoint

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,7 +1,41 @@
+import crypto from 'crypto';
 import type { FastifyInstance } from 'fastify';
 import { getLinkedinProfile } from 'getLinkedinProfile';
 import { linkedinJobQueue } from 'jobQueue';
 import { LumaClient, LumaGuest } from 'luma';
+
+class Jobs {
+  private jobs: Record<
+    string,
+    { state: 'in_progress' | 'done' | 'error'; data?: any }
+  > = {};
+
+  private async execJob(jobId: string, exec: () => void) {
+    try {
+      this.jobs[jobId].data = await exec();
+
+      this.jobs[jobId].state = 'done';
+    } catch (err) {
+      this.jobs[jobId].state = 'error';
+    }
+  }
+
+  public addJob(exec: () => void) {
+    const jobId = crypto.randomUUID();
+
+    this.jobs[jobId] = {
+      state: 'in_progress',
+    };
+    this.execJob(jobId, exec);
+    return {
+      id: jobId,
+    };
+  }
+  public getJob(jobId: string) {
+    return this.jobs[jobId];
+  }
+}
+const jobs = new Jobs();
 
 export async function routes(fastify: FastifyInstance): Promise<void> {
   fastify.get('/health', async (_, reply) => {
@@ -38,7 +72,7 @@ export async function routes(fastify: FastifyInstance): Promise<void> {
       });
     }
   });
-  fastify.post('/get-event-data', async (req, reply) => {
+  fastify.post('/luma-events', async (req, reply) => {
     try {
       const eventUrl = (req.body as any).eventUrl;
       const authToken = (req.body as any).authToken;
@@ -67,47 +101,50 @@ export async function routes(fastify: FastifyInstance): Promise<void> {
         return;
       }
 
-      let lumaGuests: LumaGuest[];
+      const job = jobs.addJob(async () => {
+        let lumaGuests: LumaGuest[];
 
-      try {
-        lumaGuests = await luma.event.getAllGuests(
-          event!.eventId,
-          event!.ticketKey
-        );
-      } catch (err) {
-        reply.code(502).send({
-          error: {
-            message: 'Failed to get event id or ticket key for event',
-          },
-        });
-        return;
-      }
+        try {
+          lumaGuests = await luma.event.getAllGuests(
+            event!.eventId,
+            event!.ticketKey
+          );
+        } catch (err) {
+          throw new Error('Failed to get event id or ticket key for event');
+          reply.code(502).send({
+            error: {
+              message: 'Failed to get event id or ticket key for event',
+            },
+          });
+          return;
+        }
 
-      const guests = await Promise.all(
-        lumaGuests!.map(async (guest) => {
-          if (!guest.linkedin_handle) {
-            return { luma: guest, linkedin: null };
-          }
-          try {
-            return await linkedinJobQueue.execute(async () => {
-              const profileData = await getLinkedinProfile(
-                guest.linkedin_handle!
+        const guests = await Promise.all(
+          lumaGuests!.map(async (guest) => {
+            if (!guest.linkedin_handle) {
+              return { luma: guest, linkedin: null };
+            }
+            try {
+              return await linkedinJobQueue.execute(async () => {
+                const profileData = await getLinkedinProfile(
+                  guest.linkedin_handle!
+                );
+
+                return { luma: guest, linkedin: profileData.data };
+              });
+            } catch (err) {
+              console.error(
+                'Failed to get linkedin profile for guest:',
+                guest,
+                err
               );
-
-              return { luma: guest, linkedin: profileData.data };
-            });
-          } catch (err) {
-            console.error(
-              'Failed to get linkedin profile for guest:',
-              guest,
-              err
-            );
-            return { luma: guest, linkedin: null };
-          }
-        })
-      );
-
-      reply.send({ data: { guests } });
+              return { luma: guest, linkedin: null };
+            }
+          })
+        );
+        return guests;
+      });
+      reply.send({ data: { task: job } });
       // eslint-disable-next-line
     } catch (err) {
       console.error(err);
@@ -118,4 +155,18 @@ export async function routes(fastify: FastifyInstance): Promise<void> {
       });
     }
   });
+  fastify.get<{ Params: { jobId: string } }>(
+    '/luma-events/:jobId',
+    async (req, reply) => {
+      const jobId = req.params.jobId as string;
+
+      const job = jobs.getJob(jobId);
+
+      if (!job) {
+        return reply.code(404).send({ error: { message: 'Not Found' } });
+      }
+
+      reply.send({ data: job });
+    }
+  );
 }
